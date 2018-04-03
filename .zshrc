d74ad53e4f049c3805b8ee1132a64f978bed3353
Dan Cardamore <dan@hld.ca>
2018-04-03 17:21:30 -0400

    Better fonts and python support

--- .zshrc
+++ .zshrc
@@ -16,6 +16,11 @@ export GIT_EDITOR=nvim
 system_type=$(uname -s)
 
 if [ "$system_type" = "Darwin" ]; then
+  # use the python's inside my user space
+  export PATH=${HOME}/Library/Python/3.6/bin:${HOME}/Library/Python/2.7/bin:${PATH}
+  # homebrew goes first
   export PATH=${HOME}/homebrew/bin:${PATH}
-elif [ "$system_type" = "Darwin" ]; then
+elif [ "$system_type" = "Linux" ]; then
 fi
+export PATH=${HOME}/bin:${PATH}
+
--- bin/dandotfiles_upgrade.sh
+++ bin/dandotfiles_upgrade.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Update system
+system_type=$(uname -s)
+if [ "$system_type" = "Darwin" ]; then
+	brew update && brew upgrade
+fi
+
+if [ "$system_type" = "Linux" ]; then
+	sudo apt-get update && sudo apt-get upgrade -u
+fi
 
 # Updating yadm submodules
 yadm submodule update --recursive
@@ -8,3 +17,5 @@ if command -v nvim >/dev/null 2>&1; then
 	echo "Updating nvim"
 	nvim '+PlugInstall' '+PlugUpdate' '+PlugClean!' '+PlugUpdate' '+PlugUpgrade' '+qall'
 fi
+
+
