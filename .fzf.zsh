d509801fb186ba3ee93466d463503fc75ce85ccc
Dan Cardamore <dan@hld.ca>
2018-04-03 19:36:17 -0400

    Working on ubuntu

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/dan/.fzf/bin* ]]; then
-  export PATH="$PATH:/Users/dan/.fzf/bin"
+if [[ ! "$PATH" == */home/dan/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/dan/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/dan/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/dan/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/dan/.fzf/shell/key-bindings.zsh"
+source "/home/dan/.fzf/shell/key-bindings.zsh"
 
