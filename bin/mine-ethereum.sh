a98fa00803e5de67b0f0dd0f9aeb33dcdf44194b
Dan Cardamore <dcardamore@apple.com>
2018-05-10 09:52:25 -0400

    fixing etherminer startup

--- bin/mine-ethereum.sh
+++ bin/mine-ethereum.sh
@@ -7,6 +7,8 @@ running=0
 
 if [ ! -e ~/log ]; then
     mkdir -f ~/log
+		rm ~/log/geth.log  # clean start
+		rm ~/log/etherminer.log  # clean start
 fi
 
 
@@ -23,10 +25,11 @@ while true; do
     else
         echo "Monitor is off" >> ~/log/geth.log
         if [ $running -eq 0 ]; then
-            echo "Starting ethminer"
+            echo "Starting ethminer" >> ~/log/geth.log
             geth --rpc --rpccorsdomain localhost 2>> ~/log/geth.log &  # Geth can always run
-            # ethminer -G
-            ethminer --farm-recheck 2000 -U -S us1.ethermine.org:4444 -FS us2.ethermine.org:4444 -O b3451a869e2c77e236a1ed75a66b1cd78a786975.neptune 2>> ~/log/geth.log &
+						sleep 20
+						echo "Starting etherminer" >> ~/log/etherminer.log
+            ethminer --farm-recheck 2000 -U -S us1.ethermine.org:4444 -FS us2.ethermine.org:4444 -O b3451a869e2c77e236a1ed75a66b1cd78a786975.neptune 2>> ~/log/etherminer.log &
             running=1
         fi
         sleep 30 # 30 seconds only when off otherwise system is unusable for 5 mins
